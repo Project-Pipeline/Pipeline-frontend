8aff002e3eb3cedcf1407307f4fc65888aa3a73e
Jing Wei Li <ljw9801055@gmail.com>
2020-08-25 23:54:36 -0400

    Initial Commit: Added Google Login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {filter} from "rxjs/operators";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+    email: string;
+    name: string;
+    id: string;
+    picture: string;
+    given_name: string;
+    last_name: string;
+    pageReady = false;
+
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.route.queryParams
+            .pipe(filter((params) => params.email != null))
+            .subscribe((param) => {
+                this.email = param.email;
+                this.name = param.name;
+                this.id = param.id;
+                this.picture = param.picture
+                this.given_name = param.given_name;
+                this.last_name = param.last_name;
+                this.pageReady = true;
+            });
+    }
+
+}
