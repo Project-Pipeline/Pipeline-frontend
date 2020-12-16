bec928955b9c254ce46739e23968baa48d5406c6
Jing Wei Li <ljw9801055@gmail.com>
2020-12-15 22:29:17 -0500

    Misc Changes & Github Actions for CI

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build](https://github.com/Project-Pipeline/Pipeline-frontend/workflows/Build/badge.svg)](https://github.com/Project-Pipeline/Pipeline-frontend/actions)
+
 # PipelineFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.1.
--- src/app/models/Global.ts
+++ src/app/models/Global.ts
@@ -3,7 +3,7 @@ import {Router} from "@angular/router";
 import {from, Observable, throwError} from "rxjs";
 
 export function handleJWTError(error: HttpErrorResponse, router: Router): Observable<any> {
-    if (error.error.reason == "JWT Error") {
+    if (error.error.reason == "malformed JWT") {
         return from(router.navigateByUrl("login?errorMessage=Your session has expired. Please re-login."));
     }
     return throwError(error);
